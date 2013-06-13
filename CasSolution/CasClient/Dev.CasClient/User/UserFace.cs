296ea3b9201c91250f8ba4ba111137b8517d227d
zbw911 <zbw911@qq.com>
2013-06-13 17:47:44 +0800

    refactor

--- CasSolution/CasClient/Dev.CasClient/User/UserFace.cs
+++ CasSolution/CasClient/Dev.CasClient/User/UserFace.cs
@@ -1,13 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace Dev.CasClient.Utils
+namespace Dev.CasClient.User
 {
 
+    /// <summary>
+    /// 取得用户头像
+    /// </summary>
     public class UserFace
     {
+
+        /// <summary>
+        /// 通过用户ID取得头像地址
+        /// </summary>
+        /// <param name="userid"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public static string GetByUserid(int userid, int type)
         {
             if (type > 4 || type < 1)
