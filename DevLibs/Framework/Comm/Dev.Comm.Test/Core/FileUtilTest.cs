07b95838e948a573ae63efd486e84fd049c54d13
zbw911 <zbw911@qq.com>
2013-12-11 11:02:07 +0800

    添加MD5时间性能

--- DevLibs/Framework/Comm/Dev.Comm.Test/Core/FileUtilTest.cs
+++ DevLibs/Framework/Comm/Dev.Comm.Test/Core/FileUtilTest.cs
@@ -27,7 +27,7 @@ namespace Dev.Comm.Test.Core
         public void Md5File2()
         {
             var filepath = @"C:\Users\Administrator\Desktop\changelog.txt";
-            var filepath2 = @"C:\Users\Administrator\Desktop\changelog1.txt";
+            var filepath2 = @"C:\Users\Administrator\Desktop\changelog.txt";
 
             var key1 = FileUtil.MD5File(filepath);
             var key2 = FileUtil.MD5Stream(filepath2);
@@ -35,6 +35,39 @@ namespace Dev.Comm.Test.Core
             Console.WriteLine(key1);
 
             Assert.AreEqual(key1, key2);
+        }
+
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            var filepath = @"E:\AutoDesk\Revit\Autodesk Revit2014完美完整包（族库、样板、注册机、序列号密、匙、安装视频及说明）.zip";
+            var key1 = FileUtil.MD5File(filepath);
+            //var key2 = FileUtil.MD5Stream(filepath);
+            sw.Stop();
+
+            Console.WriteLine(sw.ElapsedTicks);
+
+
+        }
+
+        [TestMethod]
+        public void MyTestMethod2()
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            var filepath = @"E:\AutoDesk\Revit\Autodesk Revit2014完美完整包（族库、样板、注册机、序列号密、匙、安装视频及说明）.zip";
+            //var key1 = FileUtil.MD5File(filepath);
+            var key2 = FileUtil.MD5Stream(filepath);
+
+            sw.Stop();
+
+            Console.WriteLine(sw.ElapsedTicks);
+
+
         }
     }
 }
