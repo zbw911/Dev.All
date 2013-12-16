ec1e49e36a78b8c86630b543af54d02abd73b775
zbw911 <zbw911@qq.com>
2013-12-16 15:21:08 +0800

    从 Controller 中取得特定的 Ｍｏｄｅｌ的

--- DevLibs/Framework/Comm/Dev.Comm.Core/Utils/TypeConverter.cs
+++ DevLibs/Framework/Comm/Dev.Comm.Core/Utils/TypeConverter.cs
@@ -263,7 +263,7 @@ namespace Dev.Comm.Core
         /// <returns> </returns>
         public static T ConvertType<T>(object value, T defaultValue)
         {
-            return (T) ConvertToT(value, defaultValue);
+            return (T)ConvertToT(value, defaultValue);
         }
 
         /**/
@@ -275,9 +275,100 @@ namespace Dev.Comm.Core
         /// <param name="myvalue"> 传入需要转换的值 </param>
         /// <param name="defaultValue"> 默认值 </param>
         /// <returns> </returns>
-        private static object ConvertToT<T>(object myvalue, T defaultValue)
+        public static object ConvertToT<T>(object myvalue, T defaultValue)
         {
-            TypeCode typeCode = Type.GetTypeCode(typeof (T));
+            //TypeCode typeCode = Type.GetTypeCode(typeof(T));
+            //if (myvalue == null)
+            //    return defaultValue;
+            //string value = myvalue.ToString();
+            //switch (typeCode)
+            //{
+            //    case TypeCode.Boolean:
+            //        bool flag = false;
+            //        if (bool.TryParse(value, out flag))
+            //            return flag;
+            //        return defaultValue;
+            //    case TypeCode.Char:
+            //        char c;
+            //        if (Char.TryParse(value, out c))
+            //            return c;
+            //        return defaultValue;
+            //    case TypeCode.SByte:
+            //        sbyte s = 0;
+            //        if (SByte.TryParse(value, out s))
+            //            return s;
+            //        return defaultValue;
+            //    case TypeCode.Byte:
+            //        byte b = 0;
+            //        if (Byte.TryParse(value, out b))
+            //            return b;
+            //        return defaultValue;
+            //    case TypeCode.Int16:
+            //        Int16 i16 = 0;
+            //        if (Int16.TryParse(value, out i16))
+            //            return i16;
+            //        return defaultValue;
+            //    case TypeCode.UInt16:
+            //        UInt16 ui16 = 0;
+            //        if (UInt16.TryParse(value, out ui16))
+            //            return ui16;
+            //        return defaultValue;
+            //    case TypeCode.Int32:
+            //        int i = 0;
+            //        if (Int32.TryParse(value, out i))
+            //            return i;
+            //        return defaultValue;
+            //    case TypeCode.UInt32:
+            //        UInt32 ui32 = 0;
+            //        if (UInt32.TryParse(value, out ui32))
+            //            return ui32;
+            //        return defaultValue;
+            //    case TypeCode.Int64:
+            //        Int64 i64 = 0;
+            //        if (Int64.TryParse(value, out i64))
+            //            return i64;
+            //        return defaultValue;
+            //    case TypeCode.UInt64:
+            //        UInt64 ui64 = 0;
+            //        if (UInt64.TryParse(value, out ui64))
+            //            return ui64;
+            //        return defaultValue;
+            //    case TypeCode.Single:
+            //        Single single = 0;
+            //        if (Single.TryParse(value, out single))
+            //            return single;
+            //        return defaultValue;
+            //    case TypeCode.Double:
+            //        double d = 0;
+            //        if (Double.TryParse(value, out d))
+            //            return d;
+            //        return defaultValue;
+            //    case TypeCode.Decimal:
+            //        decimal de = 0;
+            //        if (Decimal.TryParse(value, out de))
+            //            return de;
+            //        return defaultValue;
+            //    case TypeCode.DateTime:
+            //        DateTime dt;
+            //        if (DateTime.TryParse(value, out dt))
+            //            return dt;
+            //        return defaultValue;
+            //    case TypeCode.String:
+            //        if (value.Length == 0)
+            //            return "";
+            //        return value;
+            //}
+
+            return ConvertToT(myvalue, typeof(T), defaultValue);
+
+            throw new ArgumentNullException("defaultValue", "不能为Empty,Object,DBNull");
+        }
+
+
+        public static object ConvertToT(object myvalue, Type type, object defaultValue = null)
+        {
+
+            TypeCode typeCode = Type.GetTypeCode(type);
             if (myvalue == null)
                 return defaultValue;
             string value = myvalue.ToString();
@@ -287,72 +378,72 @@ namespace Dev.Comm.Core
                     bool flag = false;
                     if (bool.TryParse(value, out flag))
                         return flag;
-                    return defaultValue;
+                    return defaultValue ?? flag;
                 case TypeCode.Char:
                     char c;
                     if (Char.TryParse(value, out c))
                         return c;
-                    return defaultValue;
+                    return defaultValue ?? c;
                 case TypeCode.SByte:
                     sbyte s = 0;
                     if (SByte.TryParse(value, out s))
                         return s;
-                    return defaultValue;
+                    return defaultValue ?? s;
                 case TypeCode.Byte:
                     byte b = 0;
                     if (Byte.TryParse(value, out b))
                         return b;
-                    return defaultValue;
+                    return defaultValue ?? b;
                 case TypeCode.Int16:
                     Int16 i16 = 0;
                     if (Int16.TryParse(value, out i16))
                         return i16;
-                    return defaultValue;
+                    return defaultValue ?? i16;
                 case TypeCode.UInt16:
                     UInt16 ui16 = 0;
                     if (UInt16.TryParse(value, out ui16))
                         return ui16;
-                    return defaultValue;
+                    return defaultValue ?? ui16;
                 case TypeCode.Int32:
                     int i = 0;
                     if (Int32.TryParse(value, out i))
                         return i;
-                    return defaultValue;
+                    return defaultValue ?? i;
                 case TypeCode.UInt32:
                     UInt32 ui32 = 0;
                     if (UInt32.TryParse(value, out ui32))
                         return ui32;
-                    return defaultValue;
+                    return defaultValue ?? ui32;
                 case TypeCode.Int64:
                     Int64 i64 = 0;
                     if (Int64.TryParse(value, out i64))
                         return i64;
-                    return defaultValue;
+                    return defaultValue ?? i64;
                 case TypeCode.UInt64:
                     UInt64 ui64 = 0;
                     if (UInt64.TryParse(value, out ui64))
                         return ui64;
-                    return defaultValue;
+                    return defaultValue ?? ui64;
                 case TypeCode.Single:
                     Single single = 0;
                     if (Single.TryParse(value, out single))
                         return single;
-                    return defaultValue;
+                    return defaultValue ?? single;
                 case TypeCode.Double:
                     double d = 0;
                     if (Double.TryParse(value, out d))
                         return d;
-                    return defaultValue;
+                    return defaultValue ?? d;
                 case TypeCode.Decimal:
                     decimal de = 0;
                     if (Decimal.TryParse(value, out de))
                         return de;
-                    return defaultValue;
+                    return defaultValue ?? de;
                 case TypeCode.DateTime:
                     DateTime dt;
                     if (DateTime.TryParse(value, out dt))
                         return dt;
-                    return defaultValue;
+                    return defaultValue ?? dt;
                 case TypeCode.String:
                     if (value.Length == 0)
                         return "";
---
+++ DevLibs/Framework/Comm/Dev.Comm.Web.Mvc/Model/ModelHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Dev.Comm.Web.Mvc.Model
+{
+    /// <summary>
+    /// 模型帮助方法
+    /// </summary>
+    public class ModelHelper
+    {
+        /// <summary>
+        /// 直接取得Model
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <typeparam name="TModel"></typeparam>
+        /// <returns></returns>
+        public static TModel GetModel<TModel>(Controller controller) where TModel : class
+        {
+            return GetModel<TModel>(controller, false);
+        }
+
+        /// <summary>
+        /// 直接取得Model
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="ignoreModelStateError"></param>
+        /// <typeparam name="TModel"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="InvalidCastException"></exception>
+        public static TModel GetModel<TModel>(Controller controller, bool ignoreModelStateError) where TModel : class
+        {
+            TModel model = (TModel)DependencyResolver.Current.GetService(typeof(TModel));
+
+            if (model == null)
+            {
+                throw new ArgumentNullException("model");
+            }
+
+
+            var controllerContext = controller.ControllerContext;
+
+            var valueProvider = ValueProviderFactories.Factories.GetValueProvider(controllerContext);
+
+            IModelBinder binder = ModelBinders.Binders.GetBinder(typeof(TModel));
+
+            var innerModelState = new ModelStateDictionary();
+
+            ModelBindingContext bindingContext = new ModelBindingContext()
+            {
+                ModelMetadata = ModelMetadataProviders.Current.GetMetadataForType(() => model, typeof(TModel)),
+                //ModelName = prefix,
+                ModelState = innerModelState,
+                //PropertyFilter = propertyFilter,
+                ValueProvider = valueProvider
+            };
+
+
+
+
+            var obj = binder.BindModel(controllerContext, bindingContext);
+
+            var error = Dev.Comm.Web.Mvc.Model.ModelStateHandler.GetAllError(innerModelState);
+
+            if (error.Count() > 0)
+            {
+                throw new InvalidCastException(string.Join(",", error.Select(x => x.ErrorMessage)));
+            }
+
+            return obj as TModel;
+
+        }
+    }
+}
